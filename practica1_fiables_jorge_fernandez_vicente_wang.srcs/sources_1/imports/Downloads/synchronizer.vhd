d6347af6005be3b81c0dbf4673fdfd7e9ebe8b7a
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2025-03-18 16:05:12 +0100

    Update synchronizer.vhd

--- practica1_fiables_jorge_fernandez_vicente_wang.srcs/sources_1/imports/Downloads/synchronizer.vhd
+++ practica1_fiables_jorge_fernandez_vicente_wang.srcs/sources_1/imports/Downloads/synchronizer.vhd
@@ -1,22 +1,14 @@
--- 1 bit synchronizer based of n flip-flops for clock-domain crossings.
--- drive constant `data_in` to use as a reset synchronizer
---
--- Original author: Colm Ryan
---
--- Copyright (c) 2016 Raytheon BBN Technologies
--- Modified by pablo Sarabia to add reset in negate form
-
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity synchronizer is
   generic (
-    RESET_VALUE    : std_logic := '0'; -- reset value of all flip-flops in the chain
-    NUM_FLIP_FLOPS : natural := 2 -- number of flip-flops in the synchronizer chain
+    RESET_VALUE    : std_logic := '0'; 
+    NUM_FLIP_FLOPS : natural := 2 
   );
   port(
-    rst      : in std_logic; -- asynchronous, high-active
-    clk      : in std_logic; -- destination clock
+    rst      : in std_logic;
+    clk      : in std_logic; 
     data_in  : in std_logic;
     data_out : out std_logic
   );
@@ -24,15 +16,11 @@ end synchronizer;
 
 architecture arch of synchronizer is
 
-  --synchronizer chain of flip-flops
   signal sync_chain : std_logic_vector(NUM_FLIP_FLOPS-1 downto 0) := (others => RESET_VALUE);
 
-  -- Xilinx XST: disable shift-register LUT (SRL) extraction
   attribute shreg_extract : string;
   attribute shreg_extract of sync_chain : signal is "no";
 
-  -- Vivado: set ASYNC_REG to specify registers receive asynchronous data
-  -- also acts as DONT_TOUCH
   attribute ASYNC_REG : string;
   attribute ASYNC_REG of sync_chain : signal is "TRUE";
 
