0ebc7f604f92ac99ab2697839b08f02da5ca0a2a
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2025-03-18 16:12:08 +0100

    Update tb_top.vhd

--- practica1_fiables_jorge_fernandez_vicente_wang.srcs/sim_1/imports/Downloads/tb_top.vhd
+++ practica1_fiables_jorge_fernandez_vicente_wang.srcs/sim_1/imports/Downloads/tb_top.vhd
@@ -1,25 +1,3 @@
---------------------------------------------------------------------------------
---
--- Title       : 	Testbench for the top module
--- Design      :	
--- Author      :	Pablo Sarabia Ortiz
--- Company     :	Universidad de Nebrija
---------------------------------------------------------------------------------
--- File        : tb_debouncer.vhd
--- Generated   : 7 February 2022
---------------------------------------------------------------------------------
--- Description : This testbench based on an async signal will test if the output 
---    toggles when the duration of the debounce has finished
---    
---------------------------------------------------------------------------------
--- Revision History :
--- -----------------------------------------------------------------------------
-
---   Ver  :| Author            :| Mod. Date :|    Changes Made:
-
---   v1.0  | Pablo Sarabia     :| 07/02/22  :| First version
-
--- -----------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -37,15 +15,13 @@ architecture testBench of tb_top is
   );
 end component;
 
-  constant timer_debounce : integer := 10; --ms
-  constant freq : integer := 100_000; --KHZ
+  constant timer_debounce : integer := 10; 
+  constant freq : integer := 100_000; 
   constant clk_period : time := (1 ms/ freq);
 
-  -- Inputs 
   signal  rst_n       :   std_logic := '0';
   signal  clk         :   std_logic := '0';
   signal  BTN     :   std_logic := '0';
-  -- Output
   signal  LED   :   std_logic;
 
 begin
